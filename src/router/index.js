45579c01da9286f598532a4a4e4c00d9d911f8b3
z123x129 <1124638371@qq.com>
2020-03-04 14:24:18 +0800

    注释登录验证

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import store from '../store'
-import axios from 'axios'
+// import store from '../store'
+// import axios from 'axios'
 import RouterConfig from './modules' // 引入业务逻辑模块
 Vue.use(VueRouter);
 
-async function getRouteList(){//获取路由表 https://gl.300c.cn
-    var routeList = "";
-    await axios.get('https://gl.300c.cn/plugin/module_config/api_template/getRouteList').then((res) => {
-        routeList = res.data.data;
-    })
-    return routeList;
-}
+// async function getRouteList(){//获取路由表 https://gl.300c.cn
+//     var routeList = "";
+//     await axios.get('https://gl.300c.cn/plugin/module_config/api_template/getRouteList').then((res) => {
+//         routeList = res.data.data;
+//     })
+//     return routeList;
+// }
 
 export async function router(){
-    var routeList = await getRouteList();
-    store.commit('getRouter', routeList);
+    // var routeList = await getRouteList();
+    // store.commit('getRouter', routeList);
     const router = new VueRouter({
         scrollBehavior: () => ({ y: 0 }),
         routes: RouterConfig
