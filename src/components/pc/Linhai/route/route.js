e2d820b8b62a1fa198e3f29a55a41b4733ffde61
徐强 <1379577123@qq.com>
2020-03-05 08:44:53 +0800

    首次提交

--- src/components/pc/Linhai/route/route.js
+++ src/components/pc/Linhai/route/route.js
@@ -4,6 +4,7 @@ const Linhai_index3 = () => import('../Linhai_index3');
 const Linhai_index4 = () => import('../Linhai_index4');
 const Linhai_index5 = () => import('../Linhai_index5');
 const Linhai_index6 = () => import('../Linhai_index6');
+const Hik_video = () => import('../Hik_video');
 export default[
     { path: '/Linhai_index', name: 'Linhai_index', component: Linhai_index },
     { path: '/Linhai_index2', name: 'Linhai_index2', component: Linhai_index2 },
@@ -11,4 +12,5 @@ export default[
     { path: '/Linhai_index4', name: 'Linhai_index2', component: Linhai_index4 },
     { path: '/Linhai_index5', name: 'Linhai_index2', component: Linhai_index5 },
     { path: '/Linhai_index6', name: 'Linhai_index2', component: Linhai_index6 },
+    { path: '/Hik_video', name: 'Hik_video', component: Hik_video },
 ];
