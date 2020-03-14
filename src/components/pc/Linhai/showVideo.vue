20e45c90764410ccc546caba68abd22c7ecbcbee
Aholife <zjy@300c.cn>
2020-03-14 10:52:34 +0800

    注释多余代码（202003141052）

--- src/components/pc/Linhai/showVideo.vue
+++ src/components/pc/Linhai/showVideo.vue
@@ -103,7 +103,7 @@
                         clearTimeout(target.resizeFlag);
                     }
                     target.resizeFlag = setTimeout(function() {
-                        console.log(that.$refs.H1.$el.offsetHeight)
+                        // console.log(that.$refs.H1.$el.offsetHeight)
                         that.$refs.H1.resizeWindow(that.$refs.H1.$el.offsetHeight,that.$refs.H1.$el.offsetWidth);
                         target.resizeFlag = null;
                     }, 200);
@@ -145,7 +145,7 @@
     .demo{
         // width: 18%;
         // float: left;
-        min-width: 200px;
+        // min-width: 200px;
         padding: 10px;
         box-sizing: border-box;
         height: 100%;
@@ -156,7 +156,7 @@
         // float: left;
         // width: 82%;
         flex: 1;
-        max-width: calc(~'100% - 200px');
+        // max-width: calc(~'100% - 200px');
         height: 100%;
     }
     .amap-info-combo .keyword-input{height:auto;}
